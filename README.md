c26f383b3d0efaa825baa5892e2cf0f7d6795c3c
calix <corey.alix@infor.com>
2022-05-27 09:29:41 -0400

    decouple data and markup

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # CBT
+
 Cognitive Brain Training
 
 Asks you to explain a situation, document your undesired reaction, consider how you could have reinterpreted that situation to have a better reaction.
-It is a simple workflow, results only persist in the browser and there is no way to export/import those results between browsers.  This is by design.
+It is a simple workflow, results only persist in the browser and there is no way to export/import those results between browsers. This is by design.
 
+# Version 1.0.1
+
+Generalize question/answer format instead of hard-coding in HTML
---
+++ data/questions.ts
@@ -0,0 +1,86 @@
+const questions = [
+  {
+    id: "situation-how",
+    q: "How did this happen?",
+  },
+  {
+    id: "mood-intensity",
+    q: "how intense was the mood?",
+  },
+  {
+    id: "mood",
+    q: "What emotions did you feel at that time?",
+  },
+  {
+    id: "mood-other",
+    q: "What else did you feel?",
+  },
+  {
+    id: "sensation",
+    q: "What did you notice in your body? Where did you feel it?",
+  },
+  {
+    id: "reaction-mind",
+    q: "What went through your mind?",
+  },
+  {
+    id: "reaction-disturbance",
+    q: "What disturbed me?",
+  },
+  {
+    id: "reaction-interpretation",
+    q: "What did those thoughts mean to you or say about you or the situation? What are you responding to?",
+  },
+  {
+    id: "reaction-trigger",
+    q: "What 'button' is this pressing for you?",
+  },
+  {
+    id: "reaction-worstcasescenario",
+    q: "What would be the worst thing that could happen?",
+  },
+  {
+    id: "reinterpretation-fact",
+    q: "Take a breath...is this fact or opinion?",
+  },
+  {
+    id: "reinterpretation-objective",
+    q: "What would someone else say about this situation? What is the bigger picture?",
+  },
+  {
+    id: "reinterpretation-alternative",
+    q: "Is there another way of seeing it?",
+  },
+  {
+    id: "reinterpretation-advice",
+    q: "What advice would you give someone else?",
+  },
+  {
+    id: "overreacted-event",
+    q: "Is this reaction in proportion to the actual event?",
+    followup: {
+      id: "important",
+      q: "Explain why.",
+    },
+  },
+  {
+    id: "overreacted-importance",
+    q: "Is this really as important as it seems?",
+    followup: {
+      id: "important",
+      q: "Explain why it was so important.",
+    },
+  },
+  {
+    id: "alternative-view-exists",
+    q: "Is there another way of seeing it?",
+    followup: {
+      id: "alternative-view",
+      q: "What is it?",
+    },
+  },
+  {
+    id: "outcome",
+    q: "What could you do differently? What would be more effective?",
+  },
+];
