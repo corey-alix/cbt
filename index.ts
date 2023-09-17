fcb8009fcedcb14a9bf0c16a22c540dd0bc0a12f
Corey Alix <coreyalix@gmail.com>
2023-09-17 16:24:21 -0400

    Topical bible verses

--- index.ts
+++ index.ts
@@ -1,102 +1,103 @@
-import { questions } from "./data/questions.js";
+export { run as bible } from "./pages/bible.js"
+import { questions } from "./data/questions.js"
 
 function showArea(areas: HTMLElement[], currentStep: number) {
-  areas.forEach((e, i) => e.classList.toggle("hidden", i !== currentStep));
-  gotoNextFocusable(document.activeElement as HTMLElement);
+  areas.forEach((e, i) => e.classList.toggle("hidden", i !== currentStep))
+  gotoNextFocusable(document.activeElement as HTMLElement)
 }
 
 function trigger(eventName: string, data: any = null) {
-  const event = new CustomEvent(eventName, { detail: data });
-  document.dispatchEvent(event);
+  const event = new CustomEvent(eventName, { detail: data })
+  document.dispatchEvent(event)
 }
 
 function on(eventName: string, cb: (detail?: any) => void) {
-  document.addEventListener(eventName, (e) => cb((<any>e).detail || null));
+  document.addEventListener(eventName, (e) => cb((<any>e).detail || null))
 }
 
 export function run() {
-  buildForm();
-  autoForLabel();
-  applyBehaviors();
-  applyTriggers();
+  buildForm()
+  autoForLabel()
+  applyBehaviors()
+  applyTriggers()
 
-  let currentStep = 0;
+  let currentStep = 0
   const areas = Array.from(
     document.querySelectorAll(".wizard .wizard-step")
-  ) as HTMLElement[];
+  ) as HTMLElement[]
 
-  showArea(areas, currentStep);
+  showArea(areas, currentStep)
 
   on("next-area", () => {
     if (currentStep < areas.length - 1) {
-      showArea(areas, ++currentStep);
+      showArea(areas, ++currentStep)
     } else {
-      trigger("finish");
+      trigger("finish")
     }
-  });
+  })
 
   on("prior-area", () => {
     if (currentStep > 0) {
-      showArea(areas, --currentStep);
+      showArea(areas, --currentStep)
     }
-  });
+  })
 
   on("finish", () => {
-    const form = document.querySelector("form.wizard") as HTMLFormElement;
-    const formData = getFormData(form);
+    const form = document.querySelector("form.wizard") as HTMLFormElement
+    const formData = getFormData(form)
     const db = JSON.parse(localStorage.getItem("cbt-db") || "[]") as Array<{
-      tick: number;
-      situation: string;
-      reaction: string;
-      reinterpretation: string;
-      outcome: string;
-    }>;
+      tick: number
+      situation: string
+      reaction: string
+      reinterpretation: string
+      outcome: string
+    }>
 
     db.unshift({
       tick: Date.now(),
       ...formData,
-    });
+    })
 
-    localStorage.setItem("cbt-db", JSON.stringify(db));
+    localStorage.setItem("cbt-db", JSON.stringify(db))
 
-    location.href = "./pages/summary.html";
-  });
+    location.href = "./pages/summary.html"
+  })
 
   on("yesorno-toggler", (detail: { element: HTMLInputElement }) => {
-    const yesorno = detail.element.closest(".yesorno") as HTMLElement;
-    if (!yesorno) return;
-    const yes = detail.element.checked;
-    yesorno.classList.toggle("yes", yes);
-    yesorno.classList.toggle("no", !yes);
-  });
+    const yesorno = detail.element.closest(".yesorno") as HTMLElement
+    if (!yesorno) return
+    const yes = detail.element.checked
+    yesorno.classList.toggle("yes", yes)
+    yesorno.classList.toggle("no", !yes)
+  })
 }
 
 function applyTriggers() {
   const triggers = Array.from(
     document.querySelectorAll("[data-trigger]")
-  ) as HTMLElement[];
+  ) as HTMLElement[]
   triggers.forEach((element) => {
-    const data = element.dataset.trigger;
-    if (!data) return;
+    const data = element.dataset.trigger
+    if (!data) return
     if (element instanceof HTMLInputElement) {
       switch (element.type) {
         case "button":
-          element.addEventListener("click", () => trigger(data, { element }));
-          break;
+          element.addEventListener("click", () => trigger(data, { element }))
+          break
         case "checkbox":
-          element.addEventListener("change", () => trigger(data, { element }));
-          break;
+          element.addEventListener("change", () => trigger(data, { element }))
+          break
       }
     }
-  });
+  })
 }
 
 function applyBehaviors() {
   Object.keys(behaviors).forEach((key) => {
     document.querySelectorAll(`.${key}`).forEach((element) => {
-      behaviors[<keyof typeof behaviors>(<any>key)](element as HTMLElement);
-    });
-  });
+      behaviors[<keyof typeof behaviors>(<any>key)](element as HTMLElement)
+    })
+  })
 }
 
 const behaviors = {
@@ -104,43 +105,41 @@ const behaviors = {
     element.addEventListener("keydown", (e) => {
       if (e.key === "Enter") {
         // fake a tab keypress
-        e.preventDefault();
-        e.stopPropagation();
+        e.preventDefault()
+        e.stopPropagation()
         // go to next focusable element
-        gotoNextFocusable(element);
+        gotoNextFocusable(element)
       }
-    });
+    })
   },
   "scroll-to-top": (e: HTMLElement) => {
     // scroll the element to the top of the page
     e.addEventListener("focus", () => {
-      const label = e.previousElementSibling as HTMLLabelElement;
-      if (label?.tagName === "LABEL") label.scrollIntoView(true);
-      else e.scrollIntoView(true);
-    });
+      const label = e.previousElementSibling as HTMLLabelElement
+      if (label?.tagName === "LABEL") label.scrollIntoView(true)
+      else e.scrollIntoView(true)
+    })
   },
-};
+}
 
 function getFormData(form: HTMLFormElement) {
-  const inputs = Array.from(
-    form.querySelectorAll("[id]")
-  ) as HTMLInputElement[];
-  const formData = {} as any;
-  inputs.filter((e) => e.value).forEach((e) => (formData[e.id] = e.value));
-  return formData;
+  const inputs = Array.from(form.querySelectorAll("[id]")) as HTMLInputElement[]
+  const formData = {} as any
+  inputs.filter((e) => e.value).forEach((e) => (formData[e.id] = e.value))
+  return formData
 }
 
 function gotoNextFocusable(element: HTMLElement) {
   const elements = Array.from(
     document.querySelectorAll(".tab-on-enter")
-  ) as HTMLElement[];
-  let index = elements.indexOf(element);
+  ) as HTMLElement[]
+  let index = elements.indexOf(element)
   while (true) {
-    const next = elements[++index % elements.length];
+    const next = elements[++index % elements.length]
     // is this element visible?
     if (next.offsetParent) {
-      next.focus();
-      break;
+      next.focus()
+      break
     }
   }
 }
@@ -148,61 +147,61 @@ function gotoNextFocusable(element: HTMLElement) {
 function autoForLabel() {
   const labels = Array.from(
     document.querySelectorAll("label")
-  ) as HTMLLabelElement[];
+  ) as HTMLLabelElement[]
   labels.forEach((label) => {
-    const input = label.nextElementSibling as HTMLInputElement;
-    if (!input || !input.id) return;
-    label.setAttribute("for", input.id);
-  });
+    const input = label.nextElementSibling as HTMLInputElement
+    if (!input || !input.id) return
+    label.setAttribute("for", input.id)
+  })
 }
 
 class FormGen {
   constructor(private form: HTMLFormElement) {}
 
   render() {
-    const target = this.form.querySelector("#questions-go-here");
+    const target = this.form.querySelector("#questions-go-here")
     // <step class="wizard-step">
     questions.forEach((question) => {
-      const step = this.createStep();
-      this.form.insertBefore(step, target);
-      const label = this.createLabel(question);
+      const step = this.createStep()
+      this.form.insertBefore(step, target)
+      const label = this.createLabel(question)
 
       if (question.followup) {
         // <div class="yesorno yes">
-        const yesorno = document.createElement("div");
+        const yesorno = document.createElement("div")
         yesorno.classList.add(
           "yesorno",
           true === question.default ? "yes" : "no"
-        );
-        step.appendChild(yesorno);
-        yesorno.appendChild(label);
-        const checkbox = document.createElement("input");
-        checkbox.classList.add("checkbox", "scroll-to-top", "tab-on-enter");
-        checkbox.type = "checkbox";
-        checkbox.id = question.id;
-        checkbox.checked = !!question.default;
-        checkbox.dataset.trigger = "yesorno-toggler";
-        yesorno.appendChild(checkbox);
-        const inputs = this.renderFollowup(question.followup, yesorno);
-        inputs.forEach((input) => input.classList.add("if-yes"));
+        )
+        step.appendChild(yesorno)
+        yesorno.appendChild(label)
+        const checkbox = document.createElement("input")
+        checkbox.classList.add("checkbox", "scroll-to-top", "tab-on-enter")
+        checkbox.type = "checkbox"
+        checkbox.id = question.id
+        checkbox.checked = !!question.default
+        checkbox.dataset.trigger = "yesorno-toggler"
+        yesorno.appendChild(checkbox)
+        const inputs = this.renderFollowup(question.followup, yesorno)
+        inputs.forEach((input) => input.classList.add("if-yes"))
       } else {
-        step.appendChild(label);
-        const input = this.createInput(question);
-        step.appendChild(input);
+        step.appendChild(label)
+        const input = this.createInput(question)
+        step.appendChild(input)
       }
-    });
+    })
   }
   private createInput(question: {
-    id: string;
-    q: string;
-    followup?: undefined;
+    id: string
+    q: string
+    followup?: undefined
   }) {
-    const input = document.createElement("textarea");
-    input.classList.add(..."large input scroll-to-top tab-on-enter".split(" "));
-    input.setAttribute("id", question.id);
-    input.setAttribute("name", question.id);
-    input.setAttribute("placeholder", question.q);
-    return input;
+    const input = document.createElement("textarea")
+    input.classList.add(..."large input scroll-to-top tab-on-enter".split(" "))
+    input.setAttribute("id", question.id)
+    input.setAttribute("name", question.id)
+    input.setAttribute("placeholder", question.q)
+    return input
   }
 
   renderFollowup(
@@ -210,29 +209,29 @@ class FormGen {
     container: HTMLElement
   ) {
     return questions.map((question) => {
-      const input = this.createInput(question);
-      container.appendChild(input);
-      return input;
-    });
+      const input = this.createInput(question)
+      container.appendChild(input)
+      return input
+    })
   }
 
   private createLabel(question: { id: string; q: string }) {
-    const label = document.createElement("label");
-    label.setAttribute("for", question.id);
-    label.classList.add("question");
-    label.innerText = question.q;
-    return label;
+    const label = document.createElement("label")
+    label.setAttribute("for", question.id)
+    label.classList.add("question")
+    label.innerText = question.q
+    return label
   }
 
   private createStep() {
-    const step = document.createElement("step");
-    step.classList.add("wizard-step");
-    return step;
+    const step = document.createElement("step")
+    step.classList.add("wizard-step")
+    return step
   }
 }
 
 function buildForm() {
-  const form = document.querySelector("form.wizard") as HTMLFormElement;
-  const formGen = new FormGen(form);
-  formGen.render();
+  const form = document.querySelector("form.wizard") as HTMLFormElement
+  const formGen = new FormGen(form)
+  formGen.render()
 }
---
+++ pages/bible.ts
@@ -0,0 +1,134 @@
+type VerseType = Record<number, string>
+type ChapterType = Record<number, { verses: VerseType }>
+type BibleType = Record<string, { chapters: ChapterType }>
+
+const Bible: BibleType = {
+  Genesis: {
+    chapters: {
+      1: {
+        verses: {
+          1: "In the beginning God created the heaven and the earth.",
+        },
+      },
+    },
+  },
+  Exodus: {
+    chapters: {
+      1: {
+        verses: {
+          1: "These are the names of the sons of Israel who went to Egypt with Jacob, each with his family:",
+        },
+      },
+    },
+  },
+  Leviticus: {
+    chapters: {
+      1: {
+        verses: {
+          1: "The Lord called to Moses and spoke to him from the tent of meeting. He said,",
+        },
+      },
+    },
+  },
+  Numbers: {
+    chapters: {
+      1: {
+        verses: {
+          1: "The Lord spoke to Moses in the tent of meeting in the Desert of Sinai on the first day of the second month of the second year after the Israelites came out of Egypt. He said:",
+        },
+      },
+    },
+  },
+} as const
+
+type BookName = keyof typeof Bible
+
+const Notes = [
+  {
+    topic: "Creation",
+    book: "Genesis",
+    verse: "1:1",
+    note: "There was a beginning, which was a concept I struggled with as a child, but which is totally reasonable.  How can there not be a beginning?  Something cannot be without a cause and yet we know God is.  This is where I have to trust in God's word and not my own understanding.",
+  },
+  {
+    topic: "Slavery",
+    book: "Exodus",
+    verse: "1:1",
+    note: "The brothers that sold Joseph into slavery begin there walk into 400 years of slavery. Our bad decisions have consequences, yet God uses them for ultimate good.",
+  },
+  {
+    topic: "Faith",
+    book: "Leviticus",
+    verse: "1:1",
+    note: "God literally calls to Moses and witnessing this, the people continued to sin against God. Trust can only come by faith.  God speaking to you would not be enough.",
+  },
+  {
+    topic: "Faith",
+    book: "Numbers",
+    verse: "1:1",
+    note: "God speaks to Moses about organizing armies - God expects us to do our part even when we feel inadequate.",
+  },
+] as Array<{
+  topic: string
+  book: BookName
+  verse: string
+  note: string
+}>
+
+export function run() {
+  const topicInput = forceElement<HTMLInputElement>(
+    "topic",
+    `<input type="text" placeholder="Topic" />`
+  )
+
+  topicInput.focus()
+  topicInput.value = localStorage.getItem("topic") || ""
+  renderTopic(topicInput.value)
+
+  topicInput.addEventListener("input", () => {
+    renderTopic(topicInput.value)
+  })
+}
+
+function showBibleVerse(book: BookName, chapterVerse: string) {
+  const [chapter, verse] = chapterVerse.split(":").map((n) => parseInt(n))
+  return Bible[book].chapters[chapter].verses[verse]
+}
+
+function renderTopic(topic: string) {
+  topic = topic.toLocaleLowerCase()
+  const notes = Notes.filter((n) => n.topic.toLocaleLowerCase().includes(topic))
+  // display the notes in a grid
+  const grid = forceElement<HTMLDivElement>("grid", `<div class="grid"></div>`)
+
+  grid.innerHTML = notes
+    .map(
+      (n) => `
+    <div class="topic">
+        <div class="book">${n.book} ${n.verse}</div>
+        <div class="verse">${showBibleVerse(n.book, n.verse)}</div>
+        <div class="note">${n.note}</div>
+    </div>`
+    )
+    .join("")
+
+  if (notes.length) {
+    localStorage.setItem("topic", topic)
+  }
+}
+
+function asHtml<T extends HTMLElement>(html: string) {
+  const element = document.createElement("div")
+  element.innerHTML = html
+  return element.firstChild as T
+}
+
+function forceElement<T extends HTMLElement>(selector: string, html: string) {
+  let element = document.querySelector(`[name="${selector}"`) as T
+  if (!element) {
+    element = asHtml<T>(html)
+    element.setAttribute("name", selector)
+    document.body.appendChild(element)
+  }
+  return element as T
+}
